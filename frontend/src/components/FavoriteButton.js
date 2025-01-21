ac7c76f08771a19594990292a2ed6937e0313b15
koalaKlon <95370656+koalaKlon@users.noreply.github.com>
2025-01-21 19:47:19 +0300

    Delete frontend/src directory

--- frontend/src/components/FavoriteButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const FavoriteButton = ({ isFavorite, onToggle }) => (
-  <button onClick={onToggle}>
-    {isFavorite ? "Удалить из избранного" : "Добавить в избранное"}
-  </button>
-);
-
-export default FavoriteButton;
